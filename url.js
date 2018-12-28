a40928f174b36b88999c29684a1b6c78f0683cfc
syuaibsyuaib <syuaibsyuaib@gmail.com>
2018-12-28 12:09:41 +0800

    pindahkan script.js ke url.js, dan tambah confirm sebelum di kirim

---
+++ url.js
@@ -0,0 +1,57 @@
+var url = 'https://script.google.com/macros/s/AKfycbxVHRSVhjlHnC6500QnRrJdTX93XwIyYquvG3HIXf0jJ0LDIyQK/exec';
+var isi = document.getElementsByTagName("input");
+
+function inputsiswa() {
+    let konfirmasi = confirm('simpanmi kah?');
+    if (konfirmasi == true) {
+        let noUrut = isi[0].value;
+        let Nama = isi[1].value;
+        let Nisn = isi[2].value;
+        let JK = document.getElementsByTagName("select")[0].value;
+        let ss = 0;
+        let datasiswa = {
+            nourut: noUrut,
+            nama: Nama,
+            nisn: Nisn,
+            jk: JK,
+            index: ss
+        };
+        kirim(datasiswa);
+    }
+}
+
+function datakls() {
+    let konfirmasi = confirm('simpanmi kah?');
+    if (konfirmasi == true) {
+        let Kelas = isi[0].value;
+        let namaGuru = isi[1].value;
+        let NIP = isi[2].value;
+        let JK = document.getElementsByTagName("select")[0].value;
+        let thnAjaran = isi[3].value;
+        let ss = 1;
+        let dataguru = {
+            kls: Kelas,
+            namaGr: namaGuru,
+            nip: NIP,
+            jk: JK,
+            ta: thnAjaran,
+            index: ss
+        };
+        kirim(dataguru);
+    }
+}
+
+function kirim(params) {
+    fetch(url, {
+            method: 'POST',
+            mode: "cors",
+            body: JSON.stringify(params),
+            credentials: 'omit',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(response => console.log('Success:' + JSON.stringify(response)))
+        .catch(error => console.log('Error:' + error));
+}
