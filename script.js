84a664dcae71bbddea8f22874e95e9091f4caf4b
syuaibsyuaib <syuaibsyuaib@gmail.com>
2019-01-01 02:30:35 +0800

    fetch url saat index diload

---
+++ script.js
@@ -0,0 +1,12 @@
+var url = 'https://script.googleusercontent.com/macros/echo?user_content_key=sWyINGjoQttmn15I3LxyF_6HtH60eY_g7Xcv7HZ-EUAgXcsbtOOqe1w8QPLkPF9gti117bOEZxhbY8rIv-bdsEc_xJtBcyAtm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHQ6uEn5Fxb9yVGFBA8jPZ6AncQ_mT73P396ioXmH6QoOiyWH9UwssC1-4-if9ZjPZKaPkzdqZk4&lib=MruXFPL-DdSNFtcuBRLX8xsgXc4k5ckPl';
+
+fetch(url)
+    .then(function (res) {
+        return res.text()
+    })
+    .then(function (respon) {
+        var database = JSON.stringify(respon);
+        localStorage.setItem('database', respon);
+        db = JSON.parse(localStorage.getItem('database'));
+        //tes();
+    })
