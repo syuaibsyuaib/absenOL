c07c25638501c6fd00f5077f227462605f9853f4
hisayuki <syuaibsyuaib@gmail.com>
2019-01-02 18:53:57 +0800

    Tambah coment

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var url = 'https://script.googleusercontent.com/macros/echo?user_content_key=sWyINGjoQttmn15I3LxyF_6HtH60eY_g7Xcv7HZ-EUAgXcsbtOOqe1w8QPLkPF9gti117bOEZxhbY8rIv-bdsEc_xJtBcyAtm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHQ6uEn5Fxb9yVGFBA8jPZ6AncQ_mT73P396ioXmH6QoOiyWH9UwssC1-4-if9ZjPZKaPkzdqZk4&lib=MruXFPL-DdSNFtcuBRLX8xsgXc4k5ckPl';
 
+//1. jika dbsementara tidak ada maka download database
+//2. jika database tidak ada maka alert 'input siswa dulu'
+//2.else. copy database jd dbsementara
+
 fetch(url)
     .then(function (res) {
         return res.text()
