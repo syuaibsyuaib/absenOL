2838c3f259c1f05f6f5601b80900b17e2da1cf68
syuaibsyuaib <syuaibsyuaib@gmail.com>
2019-01-26 15:32:47 +0800

    pindah log btn[j]

--- kehadiran.js
+++ kehadiran.js
@@ -62,7 +62,7 @@ function tes() {
     //jika tombol hadir || izin || sakit || abstain diklik maka ganti isi select dan namaSiswa
     for (var j = 0; j < btn.length - 1; j++) {
         btn[j].addEventListener('click', gantiIsiDiv);
-        console.log(btn[j]);
+        
     }
 
     //jika tombol selesai ditekan maka akan muncul confirmasi simpan atau tidak?
@@ -76,6 +76,7 @@ function tes() {
 
     //ganti isi div sesuai isi select
     function gantiIsiDiv() {
+        console.log(btn[j]);
         try {
             let indexSiswa = db.findIndex(k => k[1] == span[0].innerHTML); //ambil index nama yg tampil saat ini
             span[0].innerHTML = db[indexSiswa + 1][1]; //tambah index  nama siswa sama dengan isi select 
